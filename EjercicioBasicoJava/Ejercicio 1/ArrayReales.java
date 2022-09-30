71289d02d63685542eabcc0d86b717fc8547f65d
Rafael <rafaelsojodavo@gmail.com>
2022-09-30 17:31:45 +0200

    Initial commit

---
+++ EjercicioBasicoJava/Ejercicio 1/ArrayReales.java
@@ -0,0 +1,9 @@
+public class ArrayReales {
+    double[] nums=new double[];
+
+    public interface Estadisticas{
+        double minimo();
+        double maximo();
+        double sumatorio();
+    }
+}
