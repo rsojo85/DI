dbfe68e37dfd71d779697fa6d5aae89540deddcb
Rafael <rafaelsojodavo@gmail.com>
2022-09-30 19:10:35 +0200

    Subida

--- EjercicioBasicoJava/Ejercicio2/Math3.java
+++ EjercicioBasicoJava/Ejercicio2/Math3.java
@@ -1,5 +1,50 @@
-public class Math3 {
-    final double E = 2.7182818284590452354;
-    final double PI = 3.14159265358979323846;
+import java.lang.Math;
+public class Math3 implements Extremos {
 
+
+    @Override
+    public int min(int[] a) {
+        int minimo=a[0];
+        for (int i=1; i< a.length; i++){
+            if(a[i]<minimo){
+                minimo=a[i];
+            }
+        }
+        return minimo;
+    }
+
+    @Override
+    public int max(int[] a) {
+        int maximo=a[0];
+        for (int i=1; i< a.length; i++){
+            if(a[i]>maximo){
+                maximo=a[i];
+            }
+        }
+        return maximo;
+    }
+
+    @Override
+    public double min(double[] a) {
+        double minimo=a[0];
+        for (int i=1; i< a.length; i++){
+            if(a[i]<minimo){
+                minimo=a[i];
+            }
+        }
+        return minimo;
+    }
+
+    @Override
+    public double max(double[] a) {
+        double maximo=a[0];
+        for (int i=1; i< a.length; i++){
+            if(a[i]>maximo){
+                maximo=a[i];
+            }
+        }
+        return maximo;
+    }
+
+    
 }
